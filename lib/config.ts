3798ec34756f45524cbee0c322eb24ec890c7a64
Asif Shikder <shikdernyc@gmail.com>
2023-08-24 19:26:53 -0400

    Added initial vector store

---
+++ lib/config.ts
@@ -0,0 +1,7 @@
+class Config {
+  defaultEndpoint = 'https://2re0322e7f.execute-api.us-east-1.amazonaws.com/dev' // TODO: Change this to the prod endpoint
+}
+
+const config = new Config();
+
+export default config;

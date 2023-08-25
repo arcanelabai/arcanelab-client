dcdda59d9ff5c482f79b80e532410460eae297f8
Asif Shikder <shikdernyc@gmail.com>
2023-08-25 02:18:08 -0400

    Simple refactoring to update code stylings

--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,8 @@
-import { Document } from 'langchain/dist/document';
-import { VectorStore } from 'langchain/vectorstores/base';
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
-import axios from 'axios';
-import config from './config';
-
+import { Document } from "langchain/dist/document";
+import { VectorStore } from "langchain/vectorstores/base";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import axios, { AxiosError } from "axios";
+import config from "./config";
 
 interface ArcaneLabVectorStoreConstructor {
   apiKey: string;
@@ -18,56 +17,78 @@ class ArcaneLabVectorStore extends VectorStore {
   workspaceId: string;
   vectorStoreId: string;
   private readonly apiEndpoint: string;
-  
+
   constructor(params: ArcaneLabVectorStoreConstructor) {
-    super(new OpenAIEmbeddings({
-      openAIApiKey: params.openaiAPIKey
-    }), {});
+    super(
+      new OpenAIEmbeddings({
+        openAIApiKey: params.openaiAPIKey,
+      }),
+      {}
+    );
     this.apiKey = params.apiKey;
     this.workspaceId = params.workspaceId;
     this.vectorStoreId = params.vectorStoreId;
     this.apiEndpoint = params.apiEndpoint || config.defaultEndpoint;
   }
 
-  async addVectors(vectors: number[][], documents: Document<Record<string, any>>[]): Promise<void | string[]> {
-    const path = "vectors"
+  async addVectors(
+    vectors: number[][],
+    documents: Document<Record<string, any>>[]
+  ): Promise<void | string[]> {
+    const path = "vectors";
     const response = await this.request(path, {
       vectors,
-      documents
-    });
-    return [response.id]
-  }
-
-  async addDocuments(documents: Document<Record<string, any>>[]): Promise<void | string[]> {
-    const path = "documents"
-    const response = await this.request(path, {
-      documents
+      documents,
     });
     return [response.id];
   }
 
-  async similaritySearchVectorWithScore(vector: number[], k: number, filter?: this['FilterType'] | undefined): Promise<[Document<Record<string, any>>, number][]> {
-    const path = "similarity-search-with-score"
+  async addDocuments(
+    documents: Document<Record<string, any>>[]
+  ): Promise<void | string[]> {
+    const path = "documents";
+    const response = await this.request(path, {
+      documents,
+    });
+    return [response.id];
+  }
+
+  async similaritySearchVectorWithScore(
+    vector: number[],
+    k: number,
+    filter?: this["FilterType"] | undefined
+  ): Promise<[Document<Record<string, any>>, number][]> {
+    const path = "similarity-search-with-score";
     const response = await this.request(path, {
       vector,
       topKCount: k,
-      filter
+      filter,
     });
     return response;
   }
 
-  private async request(path: string, params: any) : Promise<any> {
-    const requestPath = `${this.apiEndpoint}/workspace/${this.workspaceId}/document-store/${this.vectorStoreId}/vectors/${path}`
-    const result = await axios.post(requestPath, params, {
-      headers: {
-        'x-api-key': this.apiKey
+  private async request(path: string, params: any): Promise<any> {
+    const requestPath = `${this.apiEndpoint}/workspace/${this.workspaceId}/document-store/${this.vectorStoreId}/vectors/${path}`;
+    try {
+      const result = await axios.post(requestPath, params, {
+        headers: {
+          "x-api-key": this.apiKey,
+        },
+      });
+      return result.data;
+    } catch (e) {
+      if (e instanceof AxiosError) {
+        throw new Error(e?.response?.data.message ?? "Unknown error");
       }
-    });
-    return result.data;
+      if (e instanceof Error) {
+        throw new Error(e.message);
+      }
+      throw e;
+    }
   }
 
   _vectorstoreType(): string {
-    return "ArcaneLabVectorStore"
+    return "ArcaneLabVectorStore";
   }
 }
 
